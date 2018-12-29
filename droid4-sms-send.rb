99750d753889f1974005ae87ef0f5531f957613b
Tony Lindgren <tony@atomide.com>
2018-12-28 17:15:42 -0800

    Add support for sending SMS
    Minimal sendmail type tool for sending sms that can be used with:
    
    $ echo "message" | droid4-sms-send.rb 1239567890
    
    Note that this does not yet work with CDMA networks as that needs
    a different format for PDU. Also note this need the related kernel
    patch for ctrl-z:
    
    mfd: motmdm: Fix sending of GSM SMS that is terminated by ctrl-z
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

---
+++ droid4-sms-send.rb
@@ -0,0 +1,89 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+#
+# Send SMS messages in PDU format using droid 4 /dev/motmdm3 device
+#
+# Needs gem install mail pdu_tools
+#
+
+require "mail"
+require "pdu_tools"
+
+$device = "motmdm3"
+
+def print_help()
+  printf "usage: echo message | %s phonenumber\n", $0
+  printf "Typically used from an email client like sendmail\n"
+end
+
+def send_pdu(pdu)
+  devname = "/dev/" + $device
+
+  # Must leave out the first hex byte and write as string
+  mot_fmt = sprintf "%s%c", pdu.pdu_hex[2..-1].downcase, 0x1a
+
+  printf "Sending Motorola GSM format PDU: %s\n", mot_fmt
+
+  if !File.exists? devname
+    error = sprintf "Error: No %s found\n", devname
+    raise error
+  end
+
+  fd = File.open devname, "r+"
+  fd.write "AT+GCMGS=\n"
+  fd.flush
+  fd.write mot_fmt
+  fd.flush
+
+  rs, ws, es = IO.select([fd], [fd], [fd], 10)
+  if rs
+    rs.each do |f|
+      data = f.readpartial 1024
+      if data =~ /ERROR/
+        error = sprintf "Got error: %s\n", data
+        raise error
+      end
+      printf "Got response: %s\n", data
+    end
+  elsif es
+    error = sprintf "Error reading %s\n", devname
+    raise error
+  else
+    error = sprintf "Timeout reading %s\n", devname
+    raise error
+  end
+  fd.close
+end
+
+def encode_pdu(phone_number, body)
+  encoder = PDUTools::Encoder.new recipient: phone_number, message: body
+  pdus = encoder.encode
+
+  pdus.each do |pdu|
+    printf "Sending to %s:\n%s\n", phone_number, body
+    send_pdu pdu
+  end
+end
+
+if !ARGV[0]
+  print_help
+  exit 0
+end
+
+input = STDIN.read
+mail = Mail.new(input)
+body = mail.decoded
+if body == ""
+  body = input
+end
+
+ARGV.each do |arg|
+  if arg == "--" || arg =~ /^-t/
+    next
+  end
+
+  email = arg.split '@'
+  phone_number = email.first
+  encode_pdu phone_number, body
+end
+
